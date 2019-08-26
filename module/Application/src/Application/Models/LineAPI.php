27063680903f8bd72c3d66d8bab2c2e842f0a621
pointsopittha <point.sopittha@gmail.com>
2019-08-26 09:13:13 +0700

    Update LineAPI.php

--- module/Application/src/Application/Models/LineAPI.php
+++ module/Application/src/Application/Models/LineAPI.php
@@ -146,7 +146,7 @@ class LineAPI
                         $url = 'https://api.line.me/v2/bot/message/reply';
                         $data = [
                         'replyToken' => $replyToken,
-                        'messages' => $messages,
+                        'messages' => [$messages],
                         ];
                         $post = json_encode($data);
                         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $this->access_token);
