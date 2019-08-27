43ca66f55be555f24a0dc32550709620319ffffe
pointsopittha <point.sopittha@gmail.com>
2019-08-27 13:34:31 +0700

    Update LineAPI.php

--- module/Application/src/Application/Models/LineAPI.php
+++ module/Application/src/Application/Models/LineAPI.php
@@ -177,7 +177,7 @@ class LineAPI
                         $text = $event['message']['text'];
                         //error_log('post'.$post);
                         //$post = getDetailFromText($event['message']['text'],$replyToken);
-                        $sql = "SELECT line_msgid,line_msg,url,imageurl FROM line_msg WHERE line_msg_contentid = 1;";
+                        $sql = "SELECT line_msgid,line_msg,url,imageurl FROM line_msg WHERE line_msg_contentid = '$text';";
                         $query = $this->adapter->query($sql);
                         $results = $query->execute();
                         $resultSet = new ResultSet;
